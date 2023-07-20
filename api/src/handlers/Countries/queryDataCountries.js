7d7775e6992679dece5c6f373614300063958ea1
ferchu <fernandojosefantini7.0@gmail.com>
2023-07-20 09:48:25 -0300

    ahora si

--- api/src/handlers/Countries/queryDataCountries.js
+++ api/src/handlers/Countries/queryDataCountries.js
@@ -2,9 +2,8 @@ const { Sequelize } = require("sequelize");
 const {Country}=require("../../db");
 async function getByQueryCountries(name) {
     const pais= await Country.findOne({
-        where:{name:{[Sequelize.Op.iLike]:`%${name}%`},
+        where:{name:{[Sequelize.Op.iLike]:`%${name}%`}},
         include: [Activity]
-    },
     })
      if (!pais) {
         throw Error("No existe un pais con ese nombre")
