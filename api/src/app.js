0421b4f55e0d9a9f302780d5fcf6dbe424f7a637
ferchu <fernandojosefantini7.0@gmail.com>
2023-07-24 16:51:10 -0300

    vamos

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
+const cors = require('cors');
 require('./db.js');
 
 const server = express();
@@ -14,6 +14,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors({ origin: 'https://proyecto-individual-six.vercel.app' }));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://proyecto-individual-six.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
