5146666e76d22ad3e509a9b596698185fffb4704
ferchu <fernandojosefantini7.0@gmail.com>
2023-07-25 17:01:44 -0300

    o

--- api/src/app.js
+++ api/src/app.js
@@ -11,7 +11,7 @@ server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
+/* server.use(cookieParser()); */
 server.use(morgan('dev'));
 /* server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://proyecto-individual-six.vercel.app/'); // update to match the domain you will make the request from
