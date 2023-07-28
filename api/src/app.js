cfa855550b86c117435d1efbef0010312007479a
ferchu <fernandojosefantini7.0@gmail.com>
2023-07-28 16:17:03 -0300

    xd

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-app.use(cors({ origin: 'https://proyecto-individual-six.vercel.app' }));
+server.use(cors({ origin: 'https://proyecto-individual-six.vercel.app' }));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
